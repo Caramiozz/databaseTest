e84c604b2b6fd9662b71e42343cbcbc2c25ecb9a
Caramiozz <56002327+Caramiozz@users.noreply.github.com>
2022-01-05 22:55:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,83 +1,39 @@
-<html>
- <head>
- <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
- </head>
- <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br>
-       Email <input type="text" name="email" id="email"/></br>
-       <input type="submit" name="submit" value="Submit" />
- </form>
- <?php
- 	// DB connection info
- 	//TODO: Update the values for $host, $user, $pwd, and $db
- 	//using the values you retrieved earlier from the Azure Portal.
- 	$host = "value of Data Source";
- 	$user = "value of User Id";
- 	$pwd = "value of Password";
- 	$db = "value of Database";
- 	// Connect to database.
- 	try {
- 		$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
- 		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
- 	}
- 	catch(Exception $e){
- 		die(var_dump($e));
- 	}
- 	// Insert registration info
- 	if(!empty($_POST)) {
- 	try {
- 		$name = $_POST['name'];
- 		$email = $_POST['email'];
- 		$date = date("Y-m-d");
- 		// Insert data
- 		$sql_insert = "INSERT INTO registration_tbl (name, email, date)
- 				   VALUES (?,?,?)";
- 		$stmt = $conn->prepare($sql_insert);
- 		$stmt->bindValue(1, $name);
- 		$stmt->bindValue(2, $email);
- 		$stmt->bindValue(3, $date);
- 		$stmt->execute();
- 	}
- 	catch(Exception $e) {
- 		die(var_dump($e));
- 	}
- 	echo "<h3>Your're registered!</h3>";
- 	}
- 	// Retrieve data
- 	$sql_select = "SELECT * FROM registration_tbl";
- 	$stmt = $conn->query($sql_select);
- 	$registrants = $stmt->fetchAll();
- 	if(count($registrants) > 0) {
- 		echo "<h2>People who are registered:</h2>";
- 		echo "<table>";
- 		echo "<tr><th>Name</th>";
- 		echo "<th>Email</th>";
- 		echo "<th>Date</th></tr>";
- 		foreach($registrants as $registrant) {
- 			echo "<tr><td>".$registrant['name']."</td>";
- 			echo "<td>".$registrant['email']."</td>";
- 			echo "<td>".$registrant['date']."</td></tr>";
-     	}
-  		echo "</table>";
- 	} else {
- 		echo "<h3>No one is currently registered.</h3>";
- 	}
- ?>
- </body>
- </html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Carousel Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+   
+   <p> Hello brother </p>
+   
+  </body>
+</html>
